f8d72f4628f809b794407b68ecc3100a50ddac77
Chiehmin Wei <cmw025@bucknell.edu>
2019-04-23 09:28:17 +0100

    avg

--- src/parse_nk.py
+++ src/parse_nk.py
@@ -1054,7 +1054,7 @@ class NKChartParser(nn.Module):
                         sent_avg_embeddings.append(tmp/tmp_len)
 
                     all_embeddings += sent_avg_embeddings
-                features_packed = from_numpy(np.array(all_embeddings)).double()
+                features_packed = from_numpy(np.array(all_embeddings))
                 print(features_packed.dtype)
                 assert 1 == 2
                 
