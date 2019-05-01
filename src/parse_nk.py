86a9feaefd5aec63a9a21be4e574070fdf401f75
Chiehmin Wei <cmw025@bucknell.edu>
2019-05-01 07:46:33 +0100

    fuu

--- src/parse_nk.py
+++ src/parse_nk.py
@@ -631,6 +631,7 @@ class Encoder(nn.Module):
 
 class WeightedLayer(nn.Module):
     def __init__(self, num_layers):
+        super(WeightedLayer, self).__init__()
         self.weight = torch.nn.Parameter(torch.Tensor(num_layers, 1, 1))
 
     def reset_parameters(self):
